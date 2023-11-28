83290c242a989c5d018f92b7dd607b349b510999
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2023-11-29 06:01:22 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -240,7 +240,7 @@ coroutine.wrap(main)()
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 wait(60)
 coroutine.yield(deleteSpecificParts)()
-local frameRateThreshold = 1
+local frameRateThreshold = 2
 local function checkFrameRate()
     while task.wait() do
         local frameRate = 1 / game:GetService("RunService").RenderStepped:Wait()
