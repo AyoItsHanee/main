695c8fdc539c840096f9a6be5d0cbfadde27c961
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2023-12-02 09:13:48 +0700

    Update sell.lua

--- sell.lua
+++ sell.lua
@@ -1,4 +1,5 @@
-local inventoryPath = game:GetService("ReplicatedStorage").Player_Data[player.Name].Inventory
+local LocalPlayer = Players.LocalPlayer
+local inventoryPath = game:GetService("ReplicatedStorage").Player_Data[LocalPlayer.Name].Inventory
 local itemsFolder = inventoryPath:WaitForChild("Items")
 
 -- Define a whitelist of item names and their prices
