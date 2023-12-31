8a6f2990d30f5de0778dcd089a5f63190f54edec
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-01-01 01:43:11 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -240,7 +240,7 @@ coroutine.wrap(main)()
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 wait(300)
 local isLooping = false
-wait(3900)
+wait(3600)
 local TeleportService = game:GetService("TeleportService")
 TeleportService:Teleport(5956785391)
 else
