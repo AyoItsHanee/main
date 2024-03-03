6181a653738355fa9face85bfd89f24bda51d160
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-03-03 12:53:16 +0700

    Update sellitems.lua

--- sellitems.lua
+++ sellitems.lua
@@ -1,13 +1,4 @@
-local function getLocalPlayerUsername()
-    local player = game.Players.LocalPlayer
-    if player then
-        return player.Name
-    else
-        return nil
-    end
-end
-
-local inventoryPath = game:GetService("ReplicatedStorage").Player_Data[getLocalPlayerUsername()].Inventory
+local inventoryPath = game:GetService("ReplicatedStorage").Player_Data.HonestTested.Inventory
 local itemsFolder = inventoryPath:WaitForChild("Items")
 
 -- Define a whitelist of item names and their prices
@@ -20,42 +11,42 @@ local whitelist = {
     ["Murasakiiro Sutoraipu Haorie"] = 3500,
     ["Uncertained Times Scarf"] = 3500,
     ["Shinobu Haorie"] = 500,
-    ["Bandaged Mask"] = 500,
-    ["Dark Smoke Visor"] = 500,
-    ["Wind Necklace"] = 500,
-    ["Stylish Haorie"] = 500,
-    ["Sabito's Mask"] = 180,
-    ["Box Lantern"] = 500,
-    ["Giyu Haorie"] = 500,
-    ["Inosuke Bottom"] = 500,
-    ["Yellow Haorie"] = 500,
-    ["Tamari Necklace"] = 500,
-    ["White Scarf"] = 500,
-    ["Boar Head"] = 500,
-    ["Banigaru Mask"] = 3500,
-    ["Tigress Warding Mask"] = 3500,
-    ["Arrow Necklace"] = 500,
-    ["Akuma no ie Mask"] = 500,
-    ["Ominous Blindfold"] = 500,
-    ["Tokosen Haorie"] = 3500,
-    ["Swamp Necklace"] = 500,
-    ["Boroboro Haorie"] = 3500,
-    ["Sleeveless Dragon Haorie"] = 500,
-    ["Black Caped Scarf"] = 500,
-    ["Akai Kumo Haorie"] = 3500,
-    ["Gem Stone Eyepatch"] = 500,
-    ["Bandaged Blindfold"] = 500,
-    ["Akuma Kurenza Mask"] = 500,
-    ["Orenjibisuto Mask"] = 500,
-    ["Mist Necklace"] = 500,
-    ["Snow Haorie"] = 500,
-    ["Fox Mask"] = 500,
-    ["Tactical Black Headband"] = 500,
-    ["Tonakai Keikai Mask"] = 3500,
-    ["Tanjiro Haorie"] = 500,
-    ["Zenitsu Haorie"] = 500,
-    ["Panther Mask"] = 500,
-    ["Striped Green Scarf"] = 500
+    ["Bandaged Mask"] = 500
+	["Dark Smoke Visor"] = 500
+	["Wind Necklace"] = 500
+	["Stylish Haorie"] = 500
+	["Sabito's Mask"] = 180
+	["Box Lantern"] = 500
+	["Giyu Haorie"] = 500
+	["Inosuke Bottom"] = 500
+	["Yellow Haorie"] = 500
+	["Tamari Necklace"] = 500
+	["White Scarf"] = 500
+	["Boar Head"] = 500
+	["Banigaru Mask"] = 3500
+	["Tigress Warding Mask"] = 3500
+	["Arrow Necklace"] = 500
+	["Akuma no ie Mask"] = 500
+	["Ominous Blindfold"] = 500
+	["Tokosen Haorie"] = 3500
+	["Swamp Necklace"] = 500
+	["Boroboro Haorie"] = 3500
+	["Sleeveless Dragon Haorie"] = 500
+	["Black Caped Scarf"] = 500
+	["Akai Kumo Haorie"] = 3500
+	["Gem Stone Eyepatch"] = 500
+	["Bandaged Blindfold"] = 500
+	["Akuma Kurenza Mask"] = 500
+	["Orenjibisuto Mask"] = 500
+	["Mist Necklace"] = 500
+	["Snow Haorie"] = 500
+	["Fox Mask"] = 500
+	["Tactical Black Headband"] = 500
+	["Tonakai Keikai Mask"] = 500
+	["Tanjiro Haorie"] = 500
+	["Zenitsu Haorie"] = 500
+	["Panther Mask"] = 500
+	["Striped Green Scarf"] = 500
 }
 
 -- Initialize variables to store found item prices and total price
@@ -76,16 +67,19 @@ local function findAndCreateArgsForItems(folder)
             local settings = itemFolder:FindFirstChild("Settings")
             if settings then
                 local idValue = settings:FindFirstChild("Id")
-                if idValue and idValue:IsA("IntValue") then
+                local amountValue = settings:FindFirstChild("Amount") -- Added line to find the Amount value
+                if idValue and idValue:IsA("IntValue") and amountValue and amountValue:IsA("IntValue") then
                     local id = idValue.Value
+                    local amount = amountValue.Value -- Changed variable name from 'id' to 'amount'
+
                     print("Item:", itemName)
                     print("ID:", id)
+                    print("Amount:", amount) -- Added line to print the amount
                     foundItems[itemName] = id
-                    -- Multiply the price by the id value
-                    totalItemPrice = totalItemPrice + (price * id)
+                    totalItemPrice = totalItemPrice + price * amount -- Adjusted totalItemPrice calculation
 
                     -- Add the item ID with a value of 1 to args[1]
-                    args[1][id] = id
+                    args[1][id] = amount -- Changed id to amount
                 end
             end
         end
@@ -93,6 +87,7 @@ local function findAndCreateArgsForItems(folder)
 
     return args
 end
+
 
 -- Call the modified function with the 'Items' folder
 local args = findAndCreateArgsForItems(itemsFolder)
@@ -121,4 +116,3 @@ wait(1)
 print("Selling Items in 1 seconds")
 
 -- Run the action with the generated args
-game:GetService("ReplicatedStorage"):WaitForChild("Sell_Items_tang"):InvokeServer(unpack(args))
