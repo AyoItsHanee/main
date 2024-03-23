02a7557008be02c2860f70044f0d5dc2308167a7
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-03-23 18:04:44 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -27,7 +27,7 @@ local Dir = COREGUI:FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOver
 if placeId == 5956785391 then
 local args= {
     [1] = "join",
-    [2] = "kh1Ejsgw",
+    [2] = "6tFMEMrx",
     [3] = 13881804983
 }
 game:GetService("ReplicatedStorage").handle_privateserver:InvokeServer(unpack(args))
