a859ba51adf032c4eebdb4cc45380f6d9d3e6a52
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-05-26 11:13:41 +0700

    Update ouw.lua

--- ouw.lua
+++ ouw.lua
@@ -20,7 +20,7 @@ if placeId == 5956785391 then
     wait()
     TeleportService:Teleport(9321822839)
 elseif placeId == 9321822839 then
-    local myUsername = "BRUHBRUH1233234" -- Replace with your actual username
+    local myUsername = "ASUS_CITY1" -- Replace with your actual username
 	local function findUsername(folder, username)
     for _, child in pairs(folder:GetChildren()) do
             local playerFolder = child:FindFirstChild(username)
