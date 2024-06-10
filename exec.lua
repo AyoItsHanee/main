d50f5cce7cf4ed85d338274220c66fb785d69d14
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 13:49:45 +0700

    Update exec.lua

--- exec.lua
+++ exec.lua
@@ -1,4 +1,6 @@
 repeat wait() until game:IsLoaded()
+local cloneref = cloneref or function(o) return o end
+Players = cloneref(game:GetService("Players"))
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()
 local TeleportCheck = false
