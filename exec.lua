2e47d986bd00753696cff78ad4196bf5e281ba84
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 13:56:37 +0700

    Update exec.lua

--- exec.lua
+++ exec.lua
@@ -1,5 +1,13 @@
-repeat wait() until game:IsLoaded()
 local cloneref = cloneref or function(o) return o end
+COREGUI = cloneref(game:GetService("CoreGui"))
+Players = cloneref(game:GetService("Players"))
+if not game:IsLoaded() then
+    local notLoaded = Instance.new("Message")
+    notLoaded.Parent = COREGUI
+    notLoaded.Text = "Infinite Yield is waiting for the game to load"
+    game.Loaded:Wait()
+    notLoaded:Destroy()
+end
 Players = cloneref(game:GetService("Players"))
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 local TeleportCheck = false
