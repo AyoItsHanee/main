9afc12b07da1d56152a36a7388b43887e395c964
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 13:44:47 +0700

    Update exec.lua

--- exec.lua
+++ exec.lua
@@ -1,4 +1,10 @@
 repeat wait() until game:IsLoaded()
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
-queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()")
+local TeleportCheck = false
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+	if not TeleportCheck and queueteleport then
+		TeleportCheck = true
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()")
+	end
+end)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()
