c243f2b9f54b586fea4fe6080b0d8ba81f7d7538
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 16:00:09 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -1,79 +1,80 @@
 repeat wait() until game:IsLoaded()
-local Players = game:GetService("Players")
-local TeleportService = game:GetService("TeleportService")
-local vu = game:GetService("VirtualUser")
-Players.LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
-game.NetworkClient.ChildRemoved:Connect(function()
-  TeleportService:Teleport(5956785391)
-end)
-game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-    if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-        TeleportService:Teleport(5956785391)
-    end
-end)
-
 local function master()
 print("executed")
 local success, error = pcall(function()
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Workspace = game:GetService("Workspace")
 local placeId = game.PlaceId
-
+local TeleportService = game:GetService("TeleportService")
+			
 if placeId == 5956785391 then
+COREGUI = game:GetService("CoreGui")
+local Dir = COREGUI:FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+	Dir.DescendantAdded:Connect(function(Err)
+		if Err.Name == "ErrorTitle" then
+			Err:GetPropertyChangedSignal("Text"):Connect(function()
+				if Err.Text:sub(0, 12) == "Disconnected" then
+						Players.LocalPlayer:Kick("\nRejoining...")
+						TeleportService:Teleport(5956785391, Players.LocalPlayer)
+				end
+			end)
+		end
+	end)
 local args= {
     [1] = "join",
     [2] = "Tc939gfy",
-    [3] = 17387482786
+    [3] = 13881804983
 }
 game:GetService("ReplicatedStorage").handle_privateserver:InvokeServer(unpack(args))
-
-				
+    
 elseif placeId == 13883059853 then
+COREGUI = game:GetService("CoreGui")
+local Dir = COREGUI:FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+	Dir.DescendantAdded:Connect(function(Err)
+		if Err.Name == "ErrorTitle" then
+			Err:GetPropertyChangedSignal("Text"):Connect(function()
+				if Err.Text:sub(0, 12) == "Disconnected" then
+						Players.LocalPlayer:Kick("\nRejoining...")
+						TeleportService:Teleport(5956785391, Players.LocalPlayer)
+				end
+			end)
+		end
+	end)
 game:GetService("Players").LocalPlayer.PlayerScripts["Small_Scripts"].Gameplay["Sun_Damage"].Disabled = true
+
 local function wd()
-					while task.wait(3) do
 		local args = {
     	[1] = true
 		}
 
 		game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("war_Drums_remote"):FireServer(unpack(args))
-					end
+		wait(3)
 end
 coroutine.wrap(wd)()
-				
 -- Auto collect chest
 local function collectChest()
-   while task.wait() do
-           for _, v in pairs(game:GetService("Workspace").Debree:GetChildren()) do
-               if v.Name == "Loot_Chest" and v:FindFirstChild("Drops") then
-                   if #v.Drops:GetChildren() == 0 then
-                       v:Destroy()
-                   else
-                       for _, drop in pairs(v.Drops:GetChildren()) do
-                           local args = {
-                               [1] = drop.Name
-                           }
-
-                           v.Add_To_Inventory:InvokeServer(unpack(args))
-                           if #v.Drops:GetChildren() == 0 then
-                               v:Destroy()
-                           end
-                       end
-                   end
-               end
-           end
-   end
+    while task.wait() do
+            for _, chest in pairs(Workspace.Debree:GetChildren()) do
+                if chest.Name == "Loot_Chest" then
+                    for _, drop in pairs(chest:FindFirstChild("Drops"):GetChildren()) do
+                        chest.Add_To_Inventory:InvokeServer(drop.Name)
+                        drop:Destroy()
+                end
+            end
+        end
+    end
 end
 coroutine.wrap(collectChest)()
 
 -- Loop to initiate a skill
 local function initiateSkill()
     while task.wait() do
-        ReplicatedStorage.Remotes.To_Server.Handle_Initiate_S_:InvokeServer("skil_ting_asd", Players.LocalPlayer, "arrow_knock_back", 5)
+        local Handle_Initiate_S_ = ReplicatedStorage.Remotes.To_Server.Handle_Initiate_S_
+        Handle_Initiate_S_:InvokeServer("skil_ting_asd", Players.LocalPlayer, "arrow_knock_back", 5)
         wait(14)
     end
 end
@@ -81,43 +82,27 @@ coroutine.wrap(initiateSkill)()
 
 -- Loop to attack mobs
 local function attackMobs()
-        while task.wait(.1) do
-            local success, error = pcall(function()
-                local hitCounter = {} -- Counter for each model
-                for i, v in next, workspace.Mobs:GetDescendants() do
-                    if v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") then
-                        local modelId = v:GetFullName()
-                        -- Check if the model has a counter and initialize it if not present
-                        if not hitCounter[modelId] then
-                            hitCounter[modelId] = 0
-                        end
-                        -- Check if the model has been hit less than 2 times
-                        if hitCounter[modelId] < 2 then
-                            local humanoid = v:FindFirstChildOfClass("Humanoid")
-                            if humanoid and humanoid.Health > 0 then
-                                ReplicatedStorage.Remotes.To_Server.Handle_Initiate_S_:InvokeServer("arrow_knock_back_damage", game.Players.LocalPlayer.Character, v.HumanoidRootPart.CFrame, v, 500, 500)
-                                hitCounter[modelId] = hitCounter[modelId] + 1
-                            else
-                                -- The humanoid health is 0, change to another model
-                                -- Replace the code below with the logic to change the model
-                                --print("Model with health 0:", modelId)
-                            end
-                        end
-                        -- Check if we hit two mobs/models
-                        local hitCount = 0
-                        for _, count in pairs(hitCounter) do
-                            hitCount = hitCount + count
-                        end
-                        if hitCount >= 2 then
-                            break -- Exit the loop if we hit two mobs/models
-                        end
+    while task.wait() do
+        local hitCounter = {}
+        for _, mob in pairs(Workspace.Mobs:GetDescendants()) do
+            if mob:IsA("Model") and mob:FindFirstChild("HumanoidRootPart") then
+                local modelId = mob:GetFullName()
+
+                if not hitCounter[modelId] then
+                    hitCounter[modelId] = 0
+                end
+
+                if hitCounter[modelId] < 2 then
+                    local humanoid = mob:FindFirstChildOfClass("Humanoid")
+                    if humanoid and humanoid.Health > 0 then
+                        local Handle_Initiate_S_ = ReplicatedStorage.Remotes.To_Server.Handle_Initiate_S_
+                        Handle_Initiate_S_:InvokeServer("arrow_knock_back_damage", Players.LocalPlayer.Character, mob.HumanoidRootPart.CFrame, mob, 500, 500)
+                        hitCounter[modelId] = hitCounter[modelId] + 1
                     end
                 end
-            end)
-            if not success then
-                print("An error occurred:", error)
             end
         end
+    end
 end
 
 task.spawn(attackMobs)
@@ -127,23 +112,28 @@ task.spawn(attackMobs)
 local specificNames = {"Map", "InteractiveShopItems", "MugenTrain", "PrivateServerDummies", "cup game", "Bandage", "BeastTrainer", "BigLight", "Black Smith", "Board", "Boulder_To_Split", "Buy_Big_Gourd", "Buy_Gourd", "Buy_Medium_Gourd", "Chair", "Civilian", "Civilian 2", "ClashTrainer", "Conductor", "Customization data ting", "Demon Guy", "Demon Slayer", "Fishing_Rod2", "Flame Trainer", "Grandpa Wagwon's Wagon", "Green_Crystal", "Mae", "Malik", "Mark", "Mist Trainer", "Model", "Ouw0pp", "Part", "Patrick", "Policeman", "Rina", "RinaDesk", "Rock", "Snow Trainer", "Soryu Trainer", "Beast Trainer", "Sound Trainer", "Target_Training", "Tyrone", "potion_sails_man", "thing", "Meditate_Mat", "Push_Ups_Mat", "Union", "MeshPart", "Floor", "Mist"}
 local isLooping = true
 local function deleteSpecificParts()
-    while isLooping do
+    while task.wait() do
+	if isLooping then
             for _, part in pairs(Workspace:GetChildren()) do
                 if table.find(specificNames, part.Name) then
                     part:Destroy()
-		wait(.1)
             end
         end
     end
 end
+end
 coroutine.wrap(deleteSpecificParts)()
 
 local function preventFall()
-    while task.wait(.1) do
+    while task.wait() do
         local antifall3 = Instance.new("BodyVelocity", Players.LocalPlayer.Character.HumanoidRootPart)
         antifall3.Velocity = Vector3.new(0, 0, 0)
         antifall3.MaxForce = Vector3.new(9e9, 9e9, 9e9)
+        
+        wait() -- It's a good idea to yield control periodically to prevent performance issues
     end
+
+    -- This part should be outside the while loop
     if antifall3 then
         antifall3:Destroy()
     end
@@ -156,27 +146,80 @@ local Root = game.Players.LocalPlayer.Character.HumanoidRootPart
 local Goal = {}
 -- Function to teleport to specific paths
 local pathsToCheck = {
-    { name = "Sound Trainee", position = Vector3.new(1859, 670, -2801), path = Workspace.Mobs.Bosses.Sound_Trainee["Sound Trainee"], time = 10, num = 2 },
-    { name = "Tengen", position = Vector3.new(1463, 493, -3118), path = Workspace.Mobs.Bosses.Tengen, time = 2, num = 3 },
-    { name = "Douma", position = Vector3.new(-2, 519, -1690), path = Workspace.Mobs.Bosses.Douma, time = 5, num = 3 },
-    { name = "Renpeke", position = Vector3.new(-1289, 607, -664), path = Workspace.Mobs.Bosses["Flame Trainee"], time = 5, num = 2 },
-    { name = "Swampy", position = Vector3.new(-1349, 607, -205), path = Workspace.Mobs.Bosses.Swampy, time = 1, num = 2 },
-    { name = "Akaza", position = Vector3.new(2008, 563, -107), path = Workspace.Mobs.Bosses.Akaza, time = 10, num = 3 },
-    { name = "Inosuke", position = Vector3.new(1596, 307, -394), path = Workspace.Mobs.Bosses.Inosuke, time = 2, num = 2 },
-    { name = "Enmu", position = Vector3.new(1580, 490, -667), path = Workspace.Mobs.Bosses.Enmu, time = 2, num = 2 },
-    { name = "Rengoku", position = Vector3.new(3659, 680, -355), path = Workspace.Mobs.Bosses.Rengoku, time = 5, num = 3 },
-    { name = "Muichiro", position = Vector3.new(4512, 680, -553), path = Workspace.Mobs.Bosses.Muichiro, time = 2, num = 2 }
+    {
+        name = "Sound Trainee",
+        position = Vector3.new(1859, 670, -2801),
+        path = Workspace.Mobs.Bosses.Sound_Trainee["Sound Trainee"],
+		time = 10,
+		num = 2
+    },
+    {
+        name = "Tengen",
+        position = Vector3.new(1463, 493, -3118),
+        path = Workspace.Mobs.Bosses.Tengen,
+		time = 2,
+		num = 3
+    },
+        {
+        name = "Douma",
+        position = Vector3.new(-2, 519, -1690),
+        path = Workspace.Mobs.Bosses.Douma,
+		time = 5,
+		num = 3
+    },
+        {
+        name = "Renpeke",
+        position = Vector3.new(-1289, 607, -664),
+        path = Workspace.Mobs.Bosses["Flame Trainee"],
+		time = 5,
+		num = 2
+    },
+        {
+        name = "Swampy",
+        position = Vector3.new(-1349, 607, -205),
+        path = Workspace.Mobs.Bosses.Swampy,
+		time = 1,
+		num = 2
+    },
+        {
+        name = "Akaza",
+        position = Vector3.new(2008, 563, -107),
+        path = Workspace.Mobs.Bosses.Akaza,
+		time = 10,
+		num = 3
+    },
+        {
+        name = "Inosuke",
+        position = Vector3.new(1596, 307, -394),
+        path = Workspace.Mobs.Bosses.Inosuke,
+		time = 2,
+		num = 2
+    },
+        {
+        name = "Enmu",
+        position = Vector3.new(1580, 490, -667),
+        path = Workspace.Mobs.Bosses.Enmu,
+		time = 2,
+		num = 2
+    },
+        {
+        name = "Rengoku",
+        position = Vector3.new(3659, 680, -355),
+        path = Workspace.Mobs.Bosses.Rengoku,
+		time = 5,
+		num = 3
+    },
+        {
+        name = "Muichiro",
+        position = Vector3.new(4512, 680, -553),
+        path = Workspace.Mobs.Bosses.Muichiro,
+		time = 2,
+		num = 2
+    },
     -- Add other paths here similarly
 }
-            local function getRoot()
-                local character = Players.LocalPlayer.Character
-                if character then
-                    return character:FindFirstChild("HumanoidRootPart")
-                end
-                return nil
-            end
+
 local function CheckAndMove(pathName, position, pathToCheck, Time, Num)
-	repeat wait() until getRoot()
     print("Going to " .. pathName)
     Goal.CFrame = CFrame.new(position)
     local tween = TweenService:Create(Root, TweenInfo.new(Time, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0), Goal)
@@ -196,7 +239,7 @@ local function CheckAndMove(pathName, position, pathToCheck, Time, Num)
         if currentPosition == prevPosition then
             movementTimer = movementTimer + 1
             if movementTimer > 1 then
-                --print("Character isn't moving, stopping the tween")
+                print("Character isn't moving, stopping the tween")
                 tween:Cancel() -- Stop the tween
             end
         else
@@ -213,12 +256,14 @@ local function main()
     wait()
     main()
 end
+
 coroutine.wrap(main)()
-				
 --game:GetService("RunService"):Set3dRenderingEnabled(false)
 wait(300)
-local isLooping = false		
-wait(1800)
+local isLooping = false
+			
+wait(3600)
+local TeleportService = game:GetService("TeleportService")
 TeleportService:Teleport(5956785391)
 		
 else
