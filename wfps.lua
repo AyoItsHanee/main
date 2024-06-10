a2af1357b7f178b33e3155aaae1410e0511ce57e
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 15:23:39 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -1,15 +1,24 @@
 repeat wait() until game:IsLoaded()
+local Players = game:GetService("Players")
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+httprequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+local TeleportCheck = false
+Players.LocalPlayer.OnTeleport:Connect(function(State)
+    if not TeleportCheck and queueteleport then
+        TeleportCheck = true
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()")
+    end
+end)
 
 local function master()
 print("executed")
 local success, error = pcall(function()
-local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Workspace = game:GetService("Workspace")
 local placeId = game.PlaceId
@@ -193,7 +202,7 @@ local function deleteSpecificParts()
             for _, part in pairs(Workspace:GetChildren()) do
                 if table.find(specificNames, part.Name) then
                     part:Destroy()
-		wait(1)
+		wait(.1)
             end
         end
     end
