8657db892ae86a1f5322ba7cc124b5b881430e66
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 13:37:39 +0700

    Update exec.lua

--- exec.lua
+++ exec.lua
@@ -1,3 +1,4 @@
+repeat wait() until game:IsLoaded()
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/wfps.lua'))()
