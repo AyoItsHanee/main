38af58d058c8f8be1849094566e64eadedae6603
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-10 13:33:35 +0700

    Create exec.lua

---
+++ exec.lua
@@ -0,0 +1,2 @@
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/exe.lua'))()")
