9b0046d9f6323865024875e097e3dc9edcab65ac
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-11 07:41:42 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -1,4 +1,5 @@
 repeat wait() until game:IsLoaded()
+wait(5)
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
