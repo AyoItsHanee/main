b05a778968527c8715c3770b241d192741810e13
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-06-17 19:20:04 +0700

    Update sellitems.lua

--- sellitems.lua
+++ sellitems.lua
@@ -122,12 +122,19 @@ print("Total Price of Found Items (Divided by 25000):", result)
 local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
+-- Create a Frame
+local frame = Instance.new("Frame")
+frame.Size = UDim2.new(0, 200, 0, 100)
+frame.Position = UDim2.new(0.5, -100, 0.5, -50)
+frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+frame.BackgroundTransparency = 0.5
+frame.Parent = screenGui
 -- Create a TextLabel to display the state of KeepSC
 local label = Instance.new("TextLabel")
 label.Size = UDim2.new(1, 0, 0.5, 0)
-label.Position = UDim2.new(0,5, -100, 0.5, -50)
+label.Position = UDim2.new(0, 0, 0, 0, 0)
 label.Text = "Total Price of Found Items (Divided by 25000): " .. tostring(result)
-label.Parent = screenGui
+label.Parent = frame
 
 print("Selling Items in 20 seconds")
 wait(10)
