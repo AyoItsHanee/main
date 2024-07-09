103c79a97307fc47b6699c77a0ce493a0b8bad41
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-09 23:45:59 +0700

    Update ouwf.lua

--- ouwf.lua
+++ ouwf.lua
@@ -257,19 +257,7 @@ local rooth = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
 			end
 		end
 		coroutine.wrap(wd)()
-
-		-- Function to check if the Timer GUI is visible
-		local function isTimerGuiNotVisible()
-			local player = game:GetService("Players").LocalPlayer
-			local timerGui = player.PlayerGui:WaitForChild("top_ui"):FindFirstChild("Timer")
-
-			if timerGui and timerGui.Visible == false then
-				return false
-			else
-				return true
-			end
-		end
-
+	
 		-- Coroutine function
 		local function mainCoroutine()
 			while isTimerGuiNotVisible() do
@@ -280,7 +268,7 @@ local rooth = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
 			TeleportService:Teleport(9321822839)
 		end
 
-		local timeout = 25 -- Set the timeout in seconds (change this as needed)
+		local timeout = 30 -- Set the timeout in seconds (change this as needed)
 		-- Function to check if the Timer GUI is visible
 		local function isTimerGuiVisible()
 			local player = game:GetService("Players").LocalPlayer
