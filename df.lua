44ce9b632025b1ba4a24ef38b29a81208be323f6
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-12 19:17:35 +0700

    Update df.lua

--- df.lua
+++ df.lua
@@ -1,5 +1,4 @@
 repeat wait() until game:IsLoaded()
-local success, error = pcall(function()
 local spawn, wait = task.spawn, task.wait
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
@@ -283,9 +282,5 @@ teleportToPositions(positions)
 	else
 		print("wrong game")
 	end
-end
-	end)
-if not success then
-	print("An error occurred:", error)
 end
 master()
