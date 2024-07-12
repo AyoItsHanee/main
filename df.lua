b571819b29c3ae451c4403f578cc981d7b8426b6
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-12 22:31:58 +0700

    Update df.lua

--- df.lua
+++ df.lua
@@ -82,7 +82,7 @@ print("roblox loaded")
 		end
 		buttonx.MouseButton1Click:Connect(TpB)
 
-		queueteleport = (delta and delta.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+		queueteleport = (syn and syn.queue_on_teleport) or (delta and delta.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 		local TeleportCheck = false
 		game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
 			if KeepSC then
