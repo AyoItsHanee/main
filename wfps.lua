1110cad9f0b71cdb529a641df3597b0a87164524
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-20 15:15:30 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -1,10 +1,10 @@
 repeat wait() until game:IsLoaded()
 while true do
-	if game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	if game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
 		break
 	end
 end
-local) spawn, wait = task.spawn, task.wait
+local spawn, wait = task.spawn, task.wait
 	local bossrun = true
 	local KeepSC = true
 	local checkore = true
