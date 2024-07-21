a8880db78928a06482cf19906e72f23da1148da3
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-21 18:55:03 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -540,7 +540,7 @@ coroutine.wrap(updsore)()
 			RemoveDMG()
 			wait()
 			RemovePARTICLES()
-			game:GetService("RunService"):Set3dRenderingEnabled(false)
+			--game:GetService("RunService"):Set3dRenderingEnabled(false)
 			wait(300)
 			local isLooping = false
 
