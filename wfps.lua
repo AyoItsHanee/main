ecd3652664736e33ef8cbcb2e74cd3fb3456b469
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-21 15:24:22 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -373,7 +373,6 @@ coroutine.wrap(updsore)()
 
 			spawn(attackMobs)
 
---[[
 			-- Loop to delete specific parts
 			local specificNames = {"Map", "InteractiveShopItems", "MugenTrain", "PrivateServerDummies", "cup game", "Bandage", "BeastTrainer", "BigLight", "Black Smith", "Board", "Boulder_To_Split", "Buy_Big_Gourd", "Buy_Gourd", "Buy_Medium_Gourd", "Chair", "Civilian", "Civilian 2", "ClashTrainer", "Conductor", "Customization data ting", "Demon Guy", "Demon Slayer", "Fishing_Rod2", "Flame Trainer", "Grandpa Wagwon's Wagon", "Green_Crystal", "Mae", "Malik", "Mark", "Mist Trainer", "Model", "Ouw0pp", "Part", "Patrick", "Policeman", "Rina", "RinaDesk", "Rock", "Snow Trainer", "Soryu Trainer", "Beast Trainer", "Sound Trainer", "Target_Training", "Tyrone", "potion_sails_man", "thing", "Meditate_Mat", "Push_Ups_Mat", "Union", "MeshPart", "Floor", "Mist"}
 			local isLooping = true
@@ -389,15 +388,6 @@ coroutine.wrap(updsore)()
 				end
 			end
 			coroutine.wrap(deleteSpecificParts)()
-				]]--
-				spawn(function()
-						while true do
-				        for _,v in pairs(game.Workspace.Map:GetChildren()) do
-           					v:Destroy()
-					end
-							wait()
-						end
-					end)
 
 			local function preventFall()
 				while task.wait() do
@@ -540,7 +530,7 @@ coroutine.wrap(updsore)()
 			RemoveDMG()
 			wait()
 			RemovePARTICLES()
-			--game:GetService("RunService"):Set3dRenderingEnabled(false)
+			game:GetService("RunService"):Set3dRenderingEnabled(false)
 			wait(300)
 			local isLooping = false
 
