a6d92015b501105571f1b5ff2d211c0fe44e037b
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-22 15:45:29 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -14,6 +14,7 @@ local spawn, wait = task.spawn, task.wait
 			game:GetService("TeleportService"):Teleport(5956785391)
 		end
 	end)
+--[[
 	game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
 	if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
 		if game.PlaceId == 5956785391 then
@@ -25,6 +26,7 @@ local spawn, wait = task.spawn, task.wait
 		end
 	end
 	end)
+]]--
 		queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) or (delta and delta.queue_on_teleport)
 		local TeleportCheck = false
 		game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
