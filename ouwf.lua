eb7eb5b83ef9a77aa05d3dc63785d201e3e87c23
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-27 21:42:05 +0700

    Update ouwf.lua

--- ouwf.lua
+++ ouwf.lua
@@ -234,6 +234,7 @@ local rooth = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
 						for _, v in pairs(Workspace.Map:GetChildren()) do
 							if v:IsA("Model") and v.Name == orb.name then
 								game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v:GetModelCFrame()
+								wait(2.5)
 								spawn(cdorb)
 							end
 						end
