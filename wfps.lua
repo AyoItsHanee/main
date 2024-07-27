3993ad8d018f9a12de51b551d73d714fa99e30a3
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-07-27 18:24:29 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -148,9 +148,6 @@ print("game loaded")
 		elseif game.PlaceId == 13883059853 then
 
 			spawn(function()
-			if v:IsA("Humanoid") then
-                   		v:ChangeState(11)
-               		end
 			while true do
 				for _, v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
                				if v:IsA("BasePart") and v.CanCollide then
