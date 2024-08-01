3a418efb93b1ae3877112b219f18f3b0526c58d5
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2024-08-02 06:20:26 +0700

    Update wfps1.lua

--- wfps1.lua
+++ wfps1.lua
@@ -26,7 +26,6 @@ local spawn, wait = task.spawn, task.wait
 		end
 	end
 	end)
-]]--
 
 		queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) or (delta and delta.queue_on_teleport)
 		local TeleportCheck = false
@@ -43,6 +42,7 @@ local spawn, wait = task.spawn, task.wait
 				end
 		end
 		end)
+]]--
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
