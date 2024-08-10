ed347d310fd8b243cc60a186b4986bbf4e9d383a
AyoItsHanee <sugomakyah@gmail.com>
2024-08-10 18:17:33 +0700

    s

--- memeskill.lua
+++ memeskill.lua
@@ -1,21 +1,36 @@
 local spawn, wait = task.spawn, task.wait
+local function cdz()
+    local ass = true
+    wait(8)
+    local ass = false
+end
+local function cdx()
+    local assx = true
+    wait(10.5)
+    local assx = false
+end
+
 local function pressz()
+    if not ass then
 	local VirtualInputManager = game:GetService('VirtualInputManager')
 
 	VirtualInputManager:SendKeyEvent(true, "Z", false, game)
 	wait(3) -- Simulate key press duration
 	VirtualInputManager:SendKeyEvent(false, "Z", false, game)
+    end
 end
 local function pressx()
+    if not assx then
 	local VirtualInputManager = game:GetService('VirtualInputManager')
 
 	VirtualInputManager:SendKeyEvent(true, "X", false, game)
-	wait(0.5) -- Simulate key press duration
+	wait(0.1) -- Simulate key press duration
 	VirtualInputManager:SendKeyEvent(false, "X", false, game)
+    end
 end
 while true do
     spawn(pressz)
-    wait(4)
+    wait(3.5)
     spawn(pressx)
-    wait()
+    wait(1)
 end
