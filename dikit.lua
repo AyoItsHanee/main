198b87949cfe69808a1aefc2be763d892664c4f9
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2025-02-10 00:14:35 +0700

    Create dikit.lua

---
+++ dikit.lua
@@ -0,0 +1,17 @@
+game.NetworkClient.ChildRemoved:Connect(function()
+	if game.PlaceId == 76455837887178 then
+		game:GetService("TeleportService"):Teleport(76455837887178)
+	end
+end)
+		
+		queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) or (delta and delta.queue_on_teleport)
+		local TeleportCheck = false
+		game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+		if (not TeleportCheck) and queueteleport then
+			TeleportCheck = true
+			queueteleport("loadstring(game:HttpGet("loadstring(game:HttpGet('https://raw.githubusercontent.com/AyoItsHanee/main/main/dikit.lua'))()")
+				end
+		end
+		end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Initiate.lua"))()
