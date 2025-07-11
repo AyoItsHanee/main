94e8a906570ca49581ccb3d33d13513eecd7c904
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2025-07-11 19:00:46 +0700

    Update ouwcp.lua

--- ouwcp.lua
+++ ouwcp.lua
@@ -167,12 +167,14 @@ print("game loaded")
 		wait(1)
 
 		local function preventFall()
-				while spsn do
-					local antifall3 = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
-					antifall3.Velocity = Vector3.new(0, 0, 0)
-					antifall3.MaxForce = Vector3.new(9e9, 9e9, 9e9)
-
-					wait() -- It's a good idea to yield control periodically to prevent performance issues
+				while task.wait() do
+					if spsn then
+						local antifall3 = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
+						antifall3.Velocity = Vector3.new(0, 0, 0)
+						antifall3.MaxForce = Vector3.new(9e9, 9e9, 9e9)
+					else
+						antifall3:Destroy()
+					end
 				end
 
 				-- This part should be outside the while loop
@@ -193,13 +195,12 @@ print("game loaded")
 		wait(11)
 
 	local function noclip()
-		while true do
+		while task.wait() do
 			for _, v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
                			if v:IsA("BasePart") and v.CanCollide then
                    			v.CanCollide = false 
 				end
                		end
-			wait(0.1)
 		end
 	end
 
