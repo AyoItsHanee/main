c896fc9d6ff165cbb0d96ea5a3caaee283de0db9
AyoItsHanee <136422952+AyoItsHanee@users.noreply.github.com>
2025-08-23 11:44:28 +0700

    Update wfps.lua

--- wfps.lua
+++ wfps.lua
@@ -482,7 +482,7 @@ end)
 					position = Vector3.new(247, 526, -2987),
 					path = Workspace.Mobs.Bosses.Snow_Trainee["Snow Trainee"],
 					time = 2,
-					num = 1
+					num = 2
 				},
 				{
 					name = "Douma",
@@ -586,7 +586,7 @@ end)
                 while task.wait() do
                     if bossrun then
                         local pathInWorkspace = Workspace.Mobs:FindFirstChild(pathName)
-                        if pathToCheck and #pathToCheck:GetChildren() == Num then
+                        if pathToCheck and #pathToCheck:GetChildren() =< Num then
                             print("Moving to the next path")
                             break
                         end
