e17d426d7c227058fd0425692d59937ba59e9673
AyoItsHanee <sugomakyah@gmail.com>
2026-08-17 14:31:29 +0700

    1

--- wfps.lua
+++ wfps.lua
@@ -507,7 +507,7 @@ end)
 				while task.wait(10) do
 					if mugensekali then
 						local menit = os.date("%M")
-						--if menit == "00" or menit == "01" or menit == "02" or menit == "03" or menit == "04" or menit == "05" or menit == "06" or menit == "07" or menit == "08" or menit == "09" then
+						if menit == "00" or menit == "01" or menit == "02" or menit == "03" or menit == "04" or menit == "05" or menit == "06" or menit == "07" or menit == "08" or menit == "09" then
 							bossrun = not bossrun
 							print("turned off boss rotation")
 							repeat wait() until istweenavailable
@@ -520,7 +520,8 @@ end)
     						print("hi")
 							game:GetService("ReplicatedStorage").purchase_mugen_ticket:FireServer(1) --buy ticket
 							local mugensekali = false
-						--end
+							wait(60)
+						end
 					end
 				end
 			end
