a057861c3d2702f633499b5756113759cb9fa1b2
AyoItsHanee <sugomakyah@gmail.com>
2026-08-25 21:47:29 +0700

    1

--- testing.lua
+++ testing.lua
@@ -2911,7 +2911,7 @@ runAutofarmAttackCycle = function()
         if nearestTarget and not State.reloadingBlade then
             pcall(function()
                 root.Anchored = true
-                root.CFrame = CFrame.new(nearestTarget.nape.Position + Vector3.new(0, 300, 0))
+                root.CFrame = CFrame.new(nearestTarget.nape.Position + Vector3.new(0, 150, 0))
             end)
             if State.optimizePerformanceEnabled then
                 pcall(function() LocalPlayer.ReplicationFocus = nearestTarget.nape end)
@@ -2993,7 +2993,7 @@ local MODIFIER_TAGS = {
 ]]--
 
 local MODIFIER_TAGS = {
-    _D("aaafc31b6aaf20"), _D("aba487296eb12760"), _D("aba4872a60aa207f31"), _D("aba2c0117fae2d6127"), _D("aca5cd0c79ba6c4330236171"), _D("a6a3d51665aa2f330b226561eefd4738"), _D("a3a4c0"), _D("a2a7c60a78e30f722c22607a"), _D("b1a2ca1c2b973e7a2320")
+    _D("aaafc31b6aaf20"), _D("aba487296eb12760"), _D("aba4872a60aa207f31"), _D("aba2c0117fae2d6127"), _D("aca5cd0c79ba6c4330236171"), _D("a6a3d51665aa2f330b226561eefd4738"), _D("a3a4c0"), _D("b1a2ca1c2b973e7a2320")
 }
 
 applyAllModifiers = function()
@@ -3069,7 +3069,7 @@ retryLoop = function()
                         if leader == LocalPlayer.Name and State.autoModifiersEnabled then
 
                             --local whitelistedModifiers = { _D("aba2c0117fae2d6127"), _D("aba4872a60aa207f31"), _D("aca5cd0c79ba6c4330236171"), _D("aaafc31b6aaf20"), _D("aba487296eb12760"), _D("a3a4c0"), _D("a6a3d51665aa2f330b226561eefd4738"), _D("a2a7c60a78e30f722c22607a"), _D("aba487346eae23612b297c"), _D("b1a2ca1c2b973e7a2320") }
-                            local whitelistedModifiers = { _D("aaafc31b6aaf20"), _D("aba487296eb12760"), _D("aba2c0117fae2d6127"), _D("aba4872a60aa207f31"), _D("aca5cd0c79ba6c4330236171"), _D("a3a4c0"), _D("a6a3d51665aa2f330b226561eefd4738"), _D("a2a7c60a78e30f722c22607a"), _D("b1a2ca1c2b973e7a2320") }
+                            local whitelistedModifiers = { _D("aaafc31b6aaf20"), _D("aba487296eb12760"), _D("aba2c0117fae2d6127"), _D("aba4872a60aa207f31"), _D("aca5cd0c79ba6c4330236171"), _D("a3a4c0"), _D("a6a3d51665aa2f330b226561eefd4738"), _D("b1a2ca1c2b973e7a2320") }
                             local currentModifiers = Workspace:GetAttribute(_D("a8a4c3106daa296131")) or  _D("")
                             for _, tag in ipairs(whitelistedModifiers) do
                                 if not string.find(currentModifiers, tag, 1, true) then
@@ -3370,7 +3370,7 @@ autofarmFlyLoop = function()
                 local orbitHeight = 15
                 targetPos = nape.Position + Vector3.new(math.cos(orbitAngle) * orbitRadius, orbitHeight, math.sin(orbitAngle) * orbitRadius)
             else  
-                local yOffset = titans:FindFirstChild(_D("a4bfd31868a813472b386e7a")) and 300 or 70
+                local yOffset = titans:FindFirstChild(_D("a4bfd31868a813472b386e7a")) and 195 or 70
                 targetPos = nape.Position + Vector3.new(0, yOffset, 0)
             end
             
